eca3d8e85d50b8569252aac6adcb3915b90e0710
gabriel natan <g.souza@grouplinknetwork.com>
2022-10-23 13:16:07 -0300

    add array in List

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,16 +1,27 @@
-export const List = ()=>{
-    return(
+export const List = () => {
+    const tarefa = [
+        { 
+            tarefa: 'React',
+            tempo: '01:00:00'
+        },
+        { 
+            tarefa: 'Flutter',
+            tempo: '02:00:00'
+        }]
+    return (
         <aside>
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>02:00:00</span>
-                </li>
-                <li>
-                    <h3>Javascript</h3>
-                    <span>01:00:00</span>
-                </li>
+              {
+                tarefa.map(item=>{
+                    return(
+                        <li key={`${item.tarefa}-${item.tempo}`}>
+                            <h3>{item.tarefa}</h3>
+                            <span>{item.tempo}</span>
+                        </li>
+                    )
+                })
+              }
             </ul>
         </aside>
     )
